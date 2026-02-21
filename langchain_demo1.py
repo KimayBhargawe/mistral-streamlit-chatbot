9b90f941521818adcedacc9158a2f05157e397cc
Kimay Bhargawe <kimaybhargawe29@gmail.com>
2026-02-21 15:58:10 +0530

    Initial Commit

--- langchain_demo1.py
+++ langchain_demo1.py
@@ -1,44 +1,17 @@
-import os
 import streamlit as st
-from mistralai import Mistral
+import ollama
 
-st.title("📌 Free Mistral API Chatbot")
-MISTRAL_API_KEY = st.secrets["MISTRAL"]["api_key"]
+st.title("Local Ollama ChatBot")
 
-if not MISTRAL_API_KEY:
-    st.error("❗ Add your Mistral API key in .streamlit/secrets.toml")
-    st.stop()
+user_input = st.text_input("Ask Something")
 
-client = Mistral(api_key=MISTRAL_API_KEY)
+if st.button("Generate"):
+    if user_input:
+        response = ollama.chat(
+            model="gemma2:2b",
+            messages=[
+                {"role": "user", "content": user_input}
+            ]
+        )
 
-if "history" not in st.session_state:
-    st.session_state["history"] = []
-
-user_input = st.text_input("Enter your message:")
-
-def get_mistral_response(user_message):
-    messages = [
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": user_message}
-    ]
-
-    response = client.chat.complete(
-        model="mistral-small-latest",
-        messages=messages
-    )
-
-    return response.choices[0].message.content
-
-if user_input:
-    st.session_state.history.append({"role": "user", "content": user_input})
-
-    with st.spinner("🤖 Thinking..."):
-        reply_text = get_mistral_response(user_input)
-
-    st.session_state.history.append({"role": "assistant", "content": reply_text})
-
-for chat in st.session_state.history:
-    if chat["role"] == "user":
-        st.markdown(f"**You:** {chat['content']}")
-    else:
-        st.markdown(f"**AI:** {chat['content']}")
+        st.write(response["message"]["content"])
